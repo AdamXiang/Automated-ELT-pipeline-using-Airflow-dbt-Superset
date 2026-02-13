1650d650b638850d8aded04f2c2231a2ba49e6d8
adam <adam.chang@datarget.com>
2026-02-13 11:20:51 +0800

    feat: Superset set up

---
+++ postgres/superset_init.sql
@@ -0,0 +1,10 @@
+CREATE USER superset WITH PASSWORD 'superset';
+
+CREATE DATABASE superset_db OWNER superset;
+
+
+
+CREATE USER examples WITH PASSWORD 'examples';
+
+CREATE DATABASE examplesf_db OWNER examples;
+
